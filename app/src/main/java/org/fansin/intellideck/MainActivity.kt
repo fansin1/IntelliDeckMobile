a33f13a6982069cdae31e2849f519f327ba0524d
Fans1n <artem21481@gmail.com>
2020-05-25 23:12:43 +0300

    small refactoring

--- app/src/main/java/org/fansin/intellideck/MainActivity.kt
+++ app/src/main/java/org/fansin/intellideck/MainActivity.kt
@@ -4,18 +4,14 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.activity_main.*
-import org.fansin.intellideck.deck.domain.DeckObservable
-import org.fansin.intellideck.deck.domain.DeckRepository
-import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var deckRepository: DeckRepository
-
-    @Inject
-    lateinit var deckObservable: DeckObservable
+    private val navController: NavController
+        get() = findNavController(R.id.nav_host_fragment)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,21 +21,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
-    //TODO: Create toolbar controller
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_add -> {
-                deckObservable.onItemAdded(
-                    deckRepository.inactiveItems.first(),
-                    deckRepository.activeItems.size)
+                navController.navigate(R.id.AddCardsFragment)
                 true
             }
             else -> super.onOptionsItemSelected(item)
